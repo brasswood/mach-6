c7def9026062b6eee10ba870824fd8d8a86b7893
Andrew Riachi <andrew.riachi@gmail.com>
2025-11-02 13:57:25 -0700

    add nightly script

---
+++ nightly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e -x
+
+# Add cargo to $PATH
+export PATH=~/.cargo/bin:$PATH
+
+# Run benchmarks
+cargo bench
+
+# make report/index.html accessible in the criterion/ directory
+ln -s target/criterion/report/index.html target/criterion/
