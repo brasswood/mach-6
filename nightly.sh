f6cb4dababdad5a8ddd26932af36372a671d976a
Andrew Riachi <andrew.riachi@gmail.com>
2025-11-05 19:55:00 -0700

    add <base> tag to nightly report
    - Add <base href="report/"> to nightly report so that relative paths in
      the symlinked html work correctly
    - Force creation of symlink in case it already exists

--- nightly.sh
+++ nightly.sh
@@ -13,7 +13,10 @@ cargo bench
 # make report/index.html accessible in the criterion/ directory
 if [ -e target/criterion/report/index.html ]; then
     # create a symlink in target/criterion, which will point to "report/index.html" as interpreted from target/criterion
-    ln -s report/index.html target/criterion/
+    ln -sf report/index.html target/criterion/
+    sed -i '/<\/title>/ a\
+    <base href="report/">
+' target/criterion/index.html 
 else
     echo "<html><body>Hey! Something went wrong and <code>target/criterion/report/index.html</code> doesn't exist!</body></html>" > target/criterion/index.html
 fi
