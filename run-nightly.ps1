73ad389d70bb122a5d0c1d05fdfd9f7165c22f0c
Andrew Riachi <andrew.riachi@gmail.com>
2026-04-03 17:59:07 -0600

    add script to run nightly

---
+++ run-nightly.ps1
@@ -0,0 +1,25 @@
+$repo = "mach-6"
+$branch = "main"
+$credsPath = Join-Path $PSScriptRoot "nightly-credentials.ps1"
+if (-not (Test-Path $credsPath)) {
+    throw "Missing credentials file: $credsPath"
+}
+. $credsPath
+$basic = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${User}:${Password}"))
+$headers = @{ Authorization = "Basic $basic" }
+
+$params = @{
+    Method = 'Post'
+    Uri = 'https://nightly.cs.washington.edu//runnow'
+    Headers = $headers
+    Body = @{ repo = $repo; branch = $branch }
+    MaximumRedirection = 0
+}
+try {
+    # Commands invoked on their own print to the screen
+    Invoke-WebRequest @params
+}
+catch {
+    $_.Exception.Response
+    throw
+}
