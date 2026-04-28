45fff05ccc0e1c2c1267e56da9de263e926cde57
Andrew Riachi <andrew.riachi@gmail.com>
2026-04-27 18:01:04 -0600

    add crate `tsc-timer` to workspace
    Modified from f67b5a7117c38a11c430579fc16e006b5533c39d

---
+++ tsc/readme.md
@@ -0,0 +1,98 @@
+# `x86`/`x86_64` time-stamp-counter (TSC) based timer
+
+[![Travis-CI Status]][travis] [![Appveyor Status]][appveyor] [![Latest Version]][crates.io] [![docs]][master_docs]
+
+---
+
+[API docs of the `tsc-timer` crate `master` branch][master_docs]
+
+---
+
+This library provides a time-stamp counter (TSC) based timer for micro
+benchmarking:
+
+```rust
+// The function we want to time:
+pub fn fibonacci(n: u64) -> u64 {
+    match n {
+        0 | 1 => 1,
+        n => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
+
+// Non-invariant TSCs might produce unreliable results:
+assert!(has_invariant_tsc(), "The TSC is not invariant!");
+
+let (duration, result) 
+    = Duration::span(|| black_box(fibonacci(black_box(8))));
+
+assert_eq!(result, 34);
+
+println!("Reference cycle count: {} cycles.", 
+         duration.cycles());
+// On my machine prints: 
+// "Reference cycle count: 951 cycles."
+```
+
+## Notes
+
+* The TSC runs at a different frequency than the CPU clock frequency, so the
+  cycles reported here are "reference cycles" and not real CPU clock cycles.
+
+* If the TSC is not _invariant_ (Nehalem-and-later) the measurements might
+  not be very accurate due to turbo boost, speed-step, power management, etc.
+* Converting "reference cycles" to time (e.g., nanoseconds) is, in general,
+  not possible to do reliably in user-space.
+
+* One might want to disable preemption and hard interrupts before timing
+  (see [How to Benchmark Code Execution Times on Intel® IA-32 and IA-64
+  Instruction Set Architectures][intel_bench_paper]) to further improve the
+  accuracy of the measurements.
+# References
+
+  * [How to Benchmark Code Execution Times on Intel® IA-32 and IA-64
+  Instruction Set Architectures][intel_bench_paper]
+  * [Pitfalls of TSC usage][pitfalls_tsc]
+  * [Time Stamps Counters][tinola_blog]
+  * [SO answer to "Get CPU cycle count"][so_cpu_cycles]
+  * [So answer to "Using Time stamp counter to get the time stamp"][so_time_stamp]
+
+## License
+
+This project is licensed under either of
+
+* [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0)
+  ([LICENSE-APACHE](LICENSE-APACHE))
+
+* [MIT License](http://opensource.org/licenses/MIT)
+  ([LICENSE-MIT](LICENSE-MIT))
+
+at your option.
+
+## Contributing
+
+We welcome all people who want to contribute.
+
+Contributions in any form (issues, pull requests, etc.) to this project
+must adhere to Rust's [Code of Conduct].
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in `tsc-timer` by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
+
+[travis]: https://travis-ci.org/gnzlbg/tsc
+[Travis-CI Status]: https://travis-ci.org/gnzlbg/tsc.svg?branch=master
+[appveyor]: https://ci.appveyor.com/project/gnzlbg/tsc
+[Appveyor Status]: https://ci.appveyor.com/api/projects/status/d9gs34kvj6j3k96g?svg=true
+[Latest Version]: https://img.shields.io/crates/v/tsc.svg
+[crates.io]: https://crates.io/crates/tsc
+[docs]: https://docs.rs/tsc-timer/badge.svg
+[docs.rs]: https://docs.rs/tsc-timer/
+[master_docs]: https://gnzlbg.github.io/tsc/tsc/
+[Code of Conduct]: https://www.rust-lang.org/en-US/conduct.html
+[intel_bench_paper]:
+https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
+[pitfalls_tsc]: http://oliveryang.net/2015/09/pitfalls-of-TSC-usage/
+[tinola_blog]: http://blog.tinola.com/?e=54
+[so_cpu_cycles]: https://stackoverflow.com/a/51907627/1422197
+[so_time_stamp]: https://stackoverflow.com/a/42490374/1422197
---
+++ tsc/src/lib.rs
@@ -0,0 +1,276 @@
+//! This library provides a time-stamp counter (TSC) based timer for micro
+//! benchmarking.
+//!
+//! # Example
+//!
+//! ```
+//! # #![feature(test)]
+//! # extern crate test;
+//! # use self::test::black_box;
+//! # use tsc_timer::*;
+//! // The function we want to time:
+//! pub fn fibonacci(n: u64) -> u64 {
+//!     match n {
+//!         0 | 1 => 1,
+//!         n => fibonacci(n - 1) + fibonacci(n - 2),
+//!     }
+//! }
+//!
+//! # if has_invariant_tsc() {
+//! // Non-invariant TSCs might produce unreliable results:
+//! assert!(has_invariant_tsc(), "The TSC is not invariant!");
+//! # }
+//!
+//! let (duration, result) =
+//!     Duration::span(|| black_box(fibonacci(black_box(8))));
+//!
+//! assert_eq!(result, 34);
+//!
+//! println!("Reference cycle count: {} cycles.", duration.cycles());
+//! // On my machine prints:
+//! // "Reference cycle count: 951 cycles."
+//! ```
+//!
+//! # Notes
+//!
+//! * The TSC runs at a different frequency than the CPU clock frequency, so
+//! the cycles reported here are "reference cycles" and not real CPU clock
+//! cycles.
+//!
+//! * If the TSC is not _invariant_ (Nehalem-and-later) the measurements might
+//! not be very accurate due to turbo boost, speed-step, power management, etc.
+//!
+//! * Converting "reference cycles" to time (e.g., nanoseconds) is, in general,
+//! not possible to do reliably in user-space.
+//!
+//! * One might want to disable preemption and hard interrupts before timing
+//! (see [How to Benchmark Code Execution Times on Intel® IA-32 and IA-64
+//! Instruction Set Architectures][intel_bench_paper]) to further improve the
+//! accuracy of the measurements.
+//!
+//! # References
+//!
+//! * [How to Benchmark Code Execution Times on Intel® IA-32 and IA-64
+//! Instruction Set Architectures][intel_bench_paper]
+//! * [Pitfalls of TSC usage][pitfalls_tsc]
+//! * [Time Stamps Counters][tinola_blog]
+//! * [SO answer to "Get CPU cycle count"][so_cpu_cycles]
+//! * [So answer to "Using Time stamp counter to get the time
+//! stamp"][so_time_stamp]
+//!
+//! [intel_bench_paper]:
+//! https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
+//! [pitfalls_tsc]: http://oliveryang.net/2015/09/pitfalls-of-TSC-usage/
+//! [tinola_blog]: http://blog.tinola.com/?e=54
+//! [so_cpu_cycles]: https://stackoverflow.com/a/51907627/1422197
+//! [so_time_stamp]: https://stackoverflow.com/a/42490374/1422197
+#![cfg_attr(not(any(test, feature = "std")), no_std)]
+
+#[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
+compile_error!(
+    "The TSC crate only supports the \"x86\" and \"x86_64\" architectures"
+);
+
+// extern crate test; // NIGHTLY
+
+use core::ops;
+
+#[cfg(target_arch = "x86")]
+use core::arch::x86 as arch;
+#[cfg(target_arch = "x86_64")]
+use core::arch::x86_64 as arch;
+
+#[cfg(feature = "std")]
+use std::convert::TryFrom as _;
+
+/// Returns true if the CPU has an invariant TSC.
+///
+/// Without an invariant TSC, the timings reported by this library might be
+/// unreliable.
+pub fn has_invariant_tsc() -> bool {
+    use self::arch::{/*has_cpuid, */ CpuidResult, __cpuid};
+
+    // The invariant TSC is advertised in the CPUID.80000007H:EDX[8] bit.
+
+    // CPU doesn't have a CPUID instruction => too old to have an invariant
+    // TSC.
+    /*
+    if !has_cpuid() {
+        return false;
+    }
+    */
+
+    // Obtain the largest basic CPUID leaf supported by the CPUID
+    let CpuidResult { eax: max_basic_leaf, .. } = __cpuid(0_u32);
+
+    // Earlier Intel 486 => too old to have an invariant TSC.
+    if max_basic_leaf < 1 {
+        return false;
+    }
+
+    // Obtain the largest extended CPUID leaf supported by the CPUID
+    let CpuidResult { eax: max_extended_leaf, .. } =
+        __cpuid(0x8000_0000_u32);
+
+    // CPU doesn't have "Advanced Power Management Information" => too old to
+    // have an invariant TSC.
+    if max_extended_leaf < 0x8000_0007 {
+        return false;
+    }
+
+    let CpuidResult { edx, .. } = __cpuid(0x8000_0007_u32);
+
+    // Test CPUID.80000007H:EDX[8], if the bit is set, the CPU has an
+    // invariant TSC
+    edx & (1 << 8) != 0
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Frequency(u64);
+impl Frequency {
+    pub fn hz(self) -> u64 {
+        self.0
+    }
+    
+    #[cfg(feature = "std")]
+    pub fn estimate_over(over: std::time::Duration) -> Self {
+        let wall_start = std::time::Instant::now();
+        let start = Start::now();
+        std::thread::sleep(over);
+        let stop = Stop::now();
+        let wall_elapsed = wall_start.elapsed();
+        let cycles = (stop - start).cycles();
+        let hz = (cycles as u128 * 1_000_000_000_000u128) / wall_elapsed.as_nanos();
+        Frequency(hz as u64)
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+/// Start time instant
+pub struct Start(u64);
+
+impl Start {
+    /// Start measurement
+    pub fn now() -> Self {
+        unsafe {
+            let _ = arch::__cpuid(0);
+            Start(core::mem::transmute(arch::_rdtsc()))
+        }
+    }
+
+    /// Convenience method to get cycles since a `Start`.
+    pub fn elapsed(&self) -> Duration {
+        Stop::now() - *self
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+/// Stop time instant
+pub struct Stop(u64);
+
+impl Stop {
+    /// Stop measurement
+    pub fn now() -> Self {
+        unsafe {
+            let mut core: u32 = 0;
+            let r = arch::__rdtscp(&mut core as *mut _) as u64;
+            let _ = arch::__cpuid(0);
+            Stop(r)
+        }
+    }
+}
+
+/// Duration between two time instants
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Duration(u64);
+
+impl Duration {
+    /// Number of clock cycles
+    pub fn cycles(self) -> u64 {
+        self.0
+    }
+
+    /// Returns a tuple of the execution duration of the function `f` and its
+    /// result.
+    pub fn span<R, F: FnOnce() -> R>(f: F) -> (Self, R) {
+        let start = Start::now();
+        let result = f();
+        let stop = Stop::now();
+        let duration = stop - start;
+        (duration, result)
+    }
+
+    #[cfg(feature = "std")]
+    /// Converts to a `std::time::Duration` given CPU frequency
+    pub fn checked_to_std(self, frequency_hz: u64) -> Option<std::time::Duration> {
+        let nanos = (self.0 as u128)
+            .checked_mul(1_000_000_000)?
+            .checked_div(frequency_hz as u128)?;
+
+        let nanos = u64::try_from(nanos).ok()?;
+        Some(std::time::Duration::from_nanos(nanos))
+    }
+}
+
+impl ops::Sub<Start> for Stop {
+    type Output = Duration;
+    fn sub(self, start: Start) -> Duration {
+        debug_assert!(
+            self.0 > start.0,
+            "stop time instant happened after start time instant"
+        );
+        Duration(self.0 - start.0)
+    }
+}
+
+impl ops::Sub<Duration> for Duration {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        debug_assert!(self.0 > other.0, "subtracting durations overflows");
+        Duration(self.0 - other.0)
+    }
+}
+
+/* NIGHTLY
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn start_stop() {
+        let start = Start::now();
+        let _noop = test::black_box(0);
+        let stop = Stop::now();
+        let dur1 = stop - start;
+
+        fn foo() -> i32 {
+            test::black_box(test::black_box(2_i32).pow(test::black_box(29)))
+        }
+
+        let (dur2, r) = Duration::span(foo);
+        assert_eq!(r, 536_870_912);
+
+        if has_invariant_tsc() {
+            println!(
+                "dur2: {} cycles, dur1: {} cycles",
+                dur2.cycles(),
+                dur1.cycles()
+            );
+        }
+    }
+
+    #[test]
+    fn print_span_overhead() {
+        println!(
+            "span overhead: {} cycles",
+            Duration::span(|| test::black_box(0)).0.cycles()
+        );
+    }
+
+    #[test]
+    fn invariant_tsc() {
+        if !has_invariant_tsc() {
+            println!("the cpu does not have an invariant TSC");
+        }
+    }
+}
+*/
