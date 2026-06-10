f487a878886d9031977bfcdaf48f5e2b2963e808
Andrew Riachi <andrew.riachi@gmail.com>
2026-06-10 14:50:10 -0600

    use 0.5 seconds

--- benches/all_websites/main.rs
+++ benches/all_websites/main.rs
@@ -333,7 +333,7 @@ where
     F: Fn() -> R,
 {
     const WARM_UP_ITERATIONS: usize = 100;
-    const WARM_UP_TIME: std::time::Duration = std::time::Duration::from_secs(1);
+    const WARM_UP_TIME: std::time::Duration = std::time::Duration::from_millis(500);
     let mut samples_vec = Vec::with_capacity(num_samples as usize);
     eprint!("Benchmarking {name}...warming up for {} seconds...", WARM_UP_TIME.as_secs_f32());
     warm_up_time(&WARM_UP_TIME, &func);
